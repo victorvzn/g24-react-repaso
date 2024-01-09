b70807c12f4217659f9ecc72a477764042ad9d08
Victor Villazón <victorzn23@gmail.com>
2024-01-08 21:01:13 -0500

    trabajando en la app con rutas

--- react-budget-app-router-custom-hooks-context/src/router/Router.jsx
+++ react-budget-app-router-custom-hooks-context/src/router/Router.jsx
@@ -1,19 +1,33 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import Login from '../pages/Login'
+import Profile from '../pages/Profile'
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
+      <header className='bg-amber-200 py-4 px-6'>
+        <div className='container mx-auto flex justify-between'>
+          <h1 className='font-bold'>Budget app</h1>
+          <nav className='flex gap-3'>
+            <Link to="/" className='hover:underline'>Home</Link>
+            <Link to="/login" className='hover:underline'>Login</Link>
+            <Link to="/profile" className='hover:underline'>Profile</Link>
+          </nav>
+        </div>
+      </header>
 
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-
-        {/* TODO: Crear una ruta llamada 'profile' y que abra el componente <Profile /> */}
-
-      </Routes>
+      <main className='my-6 px-6'>
+        <div className='container mx-auto'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/profile' element={<Profile />} />
+            {/* TODO: Crear una ruta llamada 'profile' y que abra el componente <Profile /> */}
+          </Routes>
+        </div>
+      </main>
     </BrowserRouter>
   )
 }
