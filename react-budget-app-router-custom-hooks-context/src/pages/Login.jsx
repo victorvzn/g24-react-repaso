ea41cb80c898f1540133be16694d25b76ec5a298
Victor Villazón <victorzn23@gmail.com>
2024-01-08 22:12:20 -0500

    trabajando en la maquetación de la pagina de login

--- react-budget-app-router-custom-hooks-context/src/pages/Login.jsx
+++ react-budget-app-router-custom-hooks-context/src/pages/Login.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+
 const Login = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (event) => {
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    const { name, value } = event.target
+
+    setForm({ ...form, [name]: value })
+  }
+
   return (
     <form className="w-96 mx-auto">
       <h1 className="text-center text-3xl mb-8">Budget App - Login</h1>
+
+      {JSON.stringify(form)}
 
       <div className="flex flex-col gap-4 bg-slate-100 p-8 shadow-lg rounded-lg">
         <label className="font-medium">
@@ -11,6 +28,7 @@ const Login = () => {
             name="email"
             placeholder="jhon@mail.com"
             className="border w-full p-3"
+            onChange={handleChange}
           />
         </label>
 
@@ -21,6 +39,7 @@ const Login = () => {
             name="password"
             placeholder="123456789"
             className="border w-full p-3"
+            onChange={handleChange}
           />
         </label>
 
