4f2763b18d053a278e469ee9130dbba80965ecb1
Victor Villazón <victorzn23@gmail.com>
2024-01-08 22:00:52 -0500

    trabajando en la maquetación de la pagina de login

--- react-budget-app-router-custom-hooks-context/src/pages/Login.jsx
+++ react-budget-app-router-custom-hooks-context/src/pages/Login.jsx
@@ -1,6 +1,36 @@
 const Login = () => {
   return (
-    <div>Login Page</div>
+    <form className="w-96 mx-auto">
+      <h1 className="text-center text-3xl mb-8">Budget App - Login</h1>
+
+      <div className="flex flex-col gap-4 bg-slate-100 p-8 shadow-lg rounded-lg">
+        <label className="font-medium">
+          Email
+          <input
+            type="text"
+            name="email"
+            placeholder="jhon@mail.com"
+            className="border w-full p-3"
+          />
+        </label>
+
+        <label className="font-medium">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="123456789"
+            className="border w-full p-3"
+          />
+        </label>
+
+        <input
+          type="submit"
+          value="Login"
+          className="w-full bg-amber-600 p-3 font-medium hover:bg-amber-500 hover:duration-300 cursor-pointer"
+        />
+      </div>
+    </form>
   )
 }
 
