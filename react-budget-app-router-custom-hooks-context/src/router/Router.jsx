41352cce158863875d8e4c664ff544f4bc45e8d1
Victor Villazón <victorzn23@gmail.com>
2024-01-08 20:41:00 -0500

    trabajando en la app con rutas

--- react-budget-app-router-custom-hooks-context/src/router/Router.jsx
+++ react-budget-app-router-custom-hooks-context/src/router/Router.jsx
@@ -1,11 +1,18 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>HOME PAGE</h1>} />
-        <Route path='/login' element={<h1>LOGIN PAGE</h1>} />
+
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+
+        {/* TODO: Crear una ruta llamada 'profile' y que abra el componente <Profile /> */}
+
       </Routes>
     </BrowserRouter>
   )
